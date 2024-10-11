39a5cee288e9183bc251da0582d105b3e49a9af8
Renate <97016086@st.deltion.nl>
2024-10-11 16:30:56 +0200

    made some folders and an homepage and navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "../src/pages/homepage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+export default App;
