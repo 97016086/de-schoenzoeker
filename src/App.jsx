b03a264995ca366b2854a12f3c9509fb4565d193
Renate <97016086@st.deltion.nl>
2024-11-05 10:48:46 +0100

    changed the app file to have only lowercase routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Home from "./pages/homepage";
 import OverOns from "./pages/over ons";
+import Wijdtematen from "./pages/wijdtematen";
 import Schoenzoeker from "./pages/schoenzoeker";
+import SchoenSoort from "./pages/SchoenSoort";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -12,8 +14,10 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Over-ons" element={<OverOns />} />
-          <Route path="/De-SchoenZoeker" element={<Schoenzoeker />} />
+          <Route path="/over-ons" element={<OverOns />} />
+          <Route path="/wijdtematen" element={<Wijdtematen />} />
+          <Route path="/schoen-soort" element={<SchoenSoort />} />
+          <Route path="/de-schoenzoeker" element={<Schoenzoeker />} />
         </Routes>
       </div>
     </Router>
