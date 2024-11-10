e0fa9b1cf4f6b6361011ee2b742b0f122f0f9941
Renate <97016086@st.deltion.nl>
2024-11-10 13:27:20 +0100

    changed these files to no longer use the schoensoort.jsx file, because it wasnot necessary anymore

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/homepage";
 import OverOns from "./pages/over ons";
 import Wijdtematen from "./pages/wijdtematen";
 import Schoenzoeker from "./pages/schoenzoeker";
-import SchoenSoort from "./pages/SchoenSoort";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -16,7 +15,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/over-ons" element={<OverOns />} />
           <Route path="/wijdtematen" element={<Wijdtematen />} />
-          <Route path="/schoen-soort" element={<SchoenSoort />} />
           <Route path="/de-schoenzoeker" element={<Schoenzoeker />} />
         </Routes>
       </div>
