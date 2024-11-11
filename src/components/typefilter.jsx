86d3a9b563c010d665f88b26da0f83fd7785c070
Renate <97016086@st.deltion.nl>
2024-11-11 08:57:28 +0100

    made all these components into arrays that are mapped over

--- src/components/typefilter.jsx
+++ src/components/typefilter.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 
-const TypeFilter = ({ selectedTypes, handleTypeChange }) => {
+const TypeFilter = ({ toggleFilter, types, setTypes }) => {
+  const shoeTypes = [
+    { value: "sneakers", label: "Sneakers" },
+    { value: "sandals", label: "Sandalen" },
+    { value: "ankle boots", label: "Enkellaarzen" },
+    { value: "boots", label: "Laarzen" },
+    { value: "lace-up	shoes", label: "Veterschoenen" },
+    { value: "loafers", label: "Loafers" },
+    { value: "high heels", label: "Hakken" },
+  ];
   return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          value="sneakers"
-          checked={selectedTypes.includes("sneakers")}
-          onChange={handleTypeChange}
-        />
-        Sneakers
-      </label>
-      <label>
-        <input
-          type="checkbox"
-          value="ankle boots"
-          checked={selectedTypes.includes("ankle boots")}
-          onChange={handleTypeChange}
-        />
-        enkellaarzen
-      </label>
+    <div className="flex flex-col mx-8">
+      <h3 className="text-[#33a4b7]	font-display	text-base font-semibold	pt-4">
+        Type schoen
+      </h3>
+      {shoeTypes.map((type) => (
+        <label key={type.value}>
+          <input
+            type="checkbox"
+            value={type.value}
+            checked={types.includes(type.value)}
+            onChange={() => toggleFilter(types, setTypes, type.value)}
+            className="accent-[#33a4b7] font-display"
+          />
+          {type.label}
+        </label>
+      ))}
     </div>
   );
 };
