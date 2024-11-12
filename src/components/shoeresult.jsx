699bbccd20f70eba6a4b1582cb4fef8ae3fda00b
Renate <97016086@st.deltion.nl>
2024-11-12 08:59:02 +0100

    added the part that if there are no images with the google data that it shows an gray square with Geen Afbeeldingen in it

--- src/components/shoeresult.jsx
+++ src/components/shoeresult.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const ShoeResults = ({ shoes }) => {
-  console.log("Shoe results:", shoes);
+  const [searched, setSearched] = useState(false);
+
+  useEffect(() => {
+    if (shoes) setSearched(true);
+  }, [shoes]);
+
+  if (searched && (!shoes || shoes.length === 0)) {
+    return (
+      <p className="text-red-600 font-display font-medium mx-20 mt-10 text-center">
+        Geen resultaten
+      </p>
+    );
+  }
+
   return (
-    <div>
-      <h2>Resultaat:</h2>
-      <ul>
+    <>
+      <div className="flex flex-row flex-wrap gap-6 mt-14 justify-center	px-4">
         {shoes.map((shoe, index) => (
-          <li key={index}>
-            Naam: {shoe.name} - Maat: {shoe.size} Soort: {shoe.type} Wijdte:
-            {shoe.width} Kleur:
-            {shoe.color}
-          </li>
+          <a
+            key={index}
+            href={shoe.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="no-underline  bg-inherit w-40	p-2	hover:shadow-lg	transition-shadow duration-300"
+          >
+            {shoe.pagemap?.cse_image && shoe.pagemap.cse_image.length > 0 ? (
+              <img
+                src={shoe.pagemap.cse_image[0].src}
+                alt={shoe.title}
+                className="w-full h-32 object-cover	"
+              />
+            ) : (
+              <div className="w-full	h-32	bg-gray-200	flex	items-center	justify-center	">
+                <span className="text-gray-500	text-sm">Geen Afbeeldingen</span>
+              </div>
+            )}
+            <h3 className="max-w-32 ml-4">{shoe.title}</h3>
+          </a>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 };
 
